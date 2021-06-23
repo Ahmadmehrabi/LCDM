b49cb983587917344d8a9ad6cee87b3737092033
AhmaMehrabi <ahmad.mehraby@gmail.com>
2021-06-23 21:43:42 +0430

    Change Hubble function

--- background.py
+++ background.py
@@ -4,23 +4,24 @@ import scipy.integrate as integ
 
 #Here the model is defined. This is the normalized Hubble function 
 
-def normalized_hubble(z,args):
-        om0 = args[0]
+def Hubble(z,args):
+        H0 = args[0]
+        om0 = args[1]
         omm = om0*pow(1.+z,3)
         ol0 = 1 - om0 
 
-        return pow(omm + ol0 ,0.5)
+        return H0*pow(omm + ol0 ,0.5)
 
 
-def Ez_inv(z,args):
-        return 1./normalized_hubble(z,args)
+def Hub_inv(z,args):
+        return 1./Hubble(z,args)
 
 
 # This is the comoving distance
 def com_dis(z,args):
-        res = integ.quad(Ez_inv, 0., z, args=args)
-        return res[0]
+        res = integ.quad(Hub_inv, 0., z, args=args)
+        return 3e3*res[0]
 
 
-def lom_dist(z,args):
+def lom_dis(z,args):
         return (1+z)*com_dis(z,args)
