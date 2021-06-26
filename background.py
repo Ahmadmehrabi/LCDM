89a9554677545da1258cd275946abe91674ffc08
Ahmad Mehrabi <ahmad.mehraby@gmail.com>
2021-06-26 09:53:52 +0430

    Hubble function in a new format

--- background.py
+++ background.py
@@ -4,24 +4,26 @@ import scipy.integrate as integ
 
 #Here the model is defined. This is the normalized Hubble function 
 
-def Hubble(z,args):
-        H0 = args[0]
-        om0 = args[1]
-        omm = om0*pow(1.+z,3)
-        ol0 = 1 - om0 
-
+def Hubble(z,H0,om):
+        omm = om*pow(1.+z,3)
+        ol0 = 1 - om 
         return H0*pow(omm + ol0 ,0.5)
 
 
-def Hub_inv(z,args):
-        return 1./Hubble(z,args)
+def Hub_inv(z,H0,om):
+        return 1./Hubble(z,H0,om)
 
 
 # This is the comoving distance
 def com_dis(z,args):
         res = integ.quad(Hub_inv, 0., z, args=args)
-        return 3e3*res[0]
+        return 3e5*res[0]
 
 
-def lom_dis(z,args):
-        return (1+z)*com_dis(z,args)
+def mu_the(z,H0,om):
+        mu = np.zeros(len(z))
+        args = [H0,om]
+        for i in range(len(z)):
+                lsd = (1+z[i])*com_dis(z[i],args)
+                mu[i] = 5*np.log10(lsd) + 25.
+        return mu
